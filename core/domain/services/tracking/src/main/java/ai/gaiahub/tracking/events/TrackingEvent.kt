566c45ce5affec882810e07112da064e21a40232
Natan Loterio <natan_loterio@gmail.com>
2025-09-07 09:23:34 +0100

    refactor TrackingEvent interface to use property declarations

--- core/domain/services/tracking/src/main/java/ai/gaiahub/tracking/events/TrackingEvent.kt
+++ core/domain/services/tracking/src/main/java/ai/gaiahub/tracking/events/TrackingEvent.kt
@@ -3,7 +3,7 @@ package ai.gaiahub.tracking.events
 import ai.gaiahub.tracking.fields.EventField
 
 interface TrackingEvent {
-    abstract val name: EventName
-    abstract val parameters: Map<EventField, Any?>
-    abstract val timestamp: Long
+    val name: EventName
+    val parameters: Map<EventField, Any?>
+    val timestamp: Long
 }
